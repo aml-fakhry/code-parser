fe8b8c3e49c7c29147d85eec8838c9f505fffeb3
Aml Fakhri <amlfakhry13@gmail.com>
2023-03-28 17:36:02 +0200

    add the useGuard decorator

--- main.ts
+++ main.ts
@@ -1,21 +1,78 @@
 import { parse } from '@babel/parser';
-import traverse from '@babel/traverse';
+import * as t from '@babel/types';
+import traverse, { NodePath } from '@babel/traverse';
 import generate from '@babel/generator';
+import fs from 'fs';
+import path from 'path';
 
-const code = 'const n = 1';
+const options = {
+  sourceType: 'module',
+
+  plugins: ['typescript', 'decorators-legacy'],
+};
+
+const patternCode = fs.readFileSync(path.resolve('./test/pattern.in'), {
+  encoding: 'utf-8',
+});
+
+const code = fs.readFileSync(path.resolve('./test/input.in'), {
+  encoding: 'utf-8',
+});
 
 // parse the code -> ast
-const ast = parse(code);
-// transform the ast
+const patternAst = parse(patternCode, {
+  sourceType: 'module',
+
+  plugins: ['typescript', 'decorators-legacy'],
+});
+
+function extractMethodDecorator(): t.Decorator[] {
+  let decorators: t.Decorator[];
+  traverse(patternAst, {
+    enter(path) {
+      // in this example change all the variable `n` to `x`
+      if (path.isClassMethod()) {
+        decorators = path.node?.decorators ? path.node?.decorators : [];
+      }
+    },
+  });
+
+  return decorators!!;
+}
+
+const patternMethodDecorator = extractMethodDecorator();
+
+const ast = parse(code, {
+  sourceType: 'module',
+
+  plugins: ['typescript', 'decorators-legacy'],
+});
+
+function hasMethodDecorator(path: any, decoratorName: string): boolean {
+  return (
+    !!path &&
+    path.isClassMethod() &&
+    !!path.node?.decorators?.length &&
+    path.node?.decorators.some((decorator: any) => {
+      const exp = decorator?.expression as any;
+      return exp?.callee?.name === decoratorName;
+    })
+  );
+}
 traverse(ast, {
   enter(path) {
     // in this example change all the variable `n` to `x`
-    if (path.isIdentifier({ name: 'n' })) {
-      path.node.name = 'x';
+    if (path.isClassMethod() && hasMethodDecorator(path, 'Get')) {
+      if (!hasMethodDecorator(path, 'UseGuards')) {
+        path.node.decorators = [
+          ...patternMethodDecorator,
+          ...(path.node.decorators || []),
+        ];
+      }
     }
   },
 });
 
 // generate code <- ast
 const output = generate(ast);
-console.log(output.code); // 'const x = 1;'
+fs.writeFileSync(path.resolve('./test/result.out'), output.code);
